b78c347e924fbf5b1e0f42fcb5ad57c8db5019a5
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2023-11-23 15:34:55 +0100

    add:img in html code

--- standard-version/script-standard.js
+++ standard-version/script-standard.js
@@ -5,4 +5,20 @@ let itemAttivo= 0;
 
 const next = document.querySelector("#next")
 
+next.addEventListener('click', function(){
+
+    if (itemAttivo < items.length -1){
+
+        items[itemAttivo].classList.remove(".active");
+
+        itemAttivo ++;
+
+        items[itemAttivo].classList.add(".active");
+
+        if( itemAttivo === items.length -1 ){
+            next.classList.add(".end")
+        }
+    }
+});
+
 
